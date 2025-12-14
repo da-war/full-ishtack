a3113e470287fa8d559c4c8c4e9b920d4544cee6
Rana Dawar Abdullah <imdawar090@gmail.com>
2025-12-14 22:35:12 +0500

    second

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,30 @@
 import express from 'express';
-
-
+import path from 'path';
+import { ENV } from './config/env.js';
 
 const app= express();
+const __dirname = path.resolve();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+
 
 app.get('/api/v1/health', (req, res) => {
     res.status(200).json({message:"Success"});
 }); 
 
+if(ENV.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,"../admin/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
+
+    });
+}
+
+
+//make our app ready for deployment
+
+console.log(`Environment: ${ENV.NODE_ENV}`);
+
+app.listen(ENV.PORT, () => {
+    console.log('Server is running on port 3000');
+});
